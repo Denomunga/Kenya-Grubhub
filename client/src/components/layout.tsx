f9e6edd6dab155bb824559ae623b179929833e2e
Dennis <dennismungai17@gmail.com>
2025-12-29 11:39:30 +0300

    Remember Email Verification

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -486,8 +486,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </AnimatePresence>
 
       {/* Main Content */}
-      <main className="flex-1">
-        {children}
+      <main className="flex-1 min-h-0">
+        <div className="min-h-full">
+          {children}
+        </div>
       </main>
 
       {/* Footer */}
