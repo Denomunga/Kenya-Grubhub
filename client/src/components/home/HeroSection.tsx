e63fb1ba99f92371afa70249840e127ad791fc43
Dennis <dennismungai17@gmail.com>
2026-01-03 18:29:05 +0300

    i hope last one

--- client/src/components/home/HeroSection.tsx
+++ client/src/components/home/HeroSection.tsx
@@ -3,7 +3,7 @@ import { ArrowRight, Sparkles, Gift, TreePine } from "lucide-react";
 import { Link } from 'wouter';
 import { motion } from "framer-motion";
 import { useChristmas } from "@/lib/christmas";
-import heroImage from "@assets/generated_images/abcde.jpg";
+import heroImage from "@assets/generated_images/abcdef.jpg";
 const HeroSection = () => {
   const { isChristmasMode } = useChristmas();
 
